6a0f754589e2b67304e05c5f7637c68be850c501
Clark Joy Pesco <siezmicrush@gmail.com>
2021-05-21 19:30:43 +0800

    Project Starter

---
+++ lib/constants/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kScaffoldBGColor = Color(0xFF121015);
+const kActiveColor = Color(0xFF1F1E25);
+const kInActiveColor = Color(0xFF121015);
+const kTextColor = Color(0xFFFCFCFC);
+const kButtonColor = Color(0xFF1F1E25);
+const kButtonBGColor = Color(0xFF121015);
+const kButtonLabelColor = Color(0xFFFEFEFE);
+const kIconColor = Color(0xFFE8E8E8);
+const kMainTextColor = Color(0xFFFFFFFF);
+const kSubTextColor = Color(0xFFDEE2E6);
+
+const kFirstColor = Color(0xFFA370F7);
+const kSecondColor = Color(0xFF84DCC6);
+const kThirdColor = Color(0xFFF5D547);
+const kFourthColor = Color(0xFFF25F5C);
